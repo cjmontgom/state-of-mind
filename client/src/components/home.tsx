e1ecc52fd4279cb6d4f68ab708c7b9b046ecc7da
Chloe Montgomery <chloemontgomery01@gmail.com>
2020-05-04 09:38:06 +0100

    spelling mistake fix

--- client/src/components/home.tsx
+++ client/src/components/home.tsx
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         <Section>
             <SubHeader>Welcome to State Of Mind</SubHeader>
-            This app is designed for you to reflect and check in with yourself on a day to day, or even more regular basic.
+            This app is designed for you to reflect and check in with yourself on a day to day, or even more regular basis.
             Over time, the Insights page becomes useful to give you an overall idea of your longer term mental health.
             Often it's easy to lose track of our well-being in the chaos of daily life.
             Hopefully this simple tracker will help you to get a better sense of your individual mental health needs.
